acd38a74649e37a5345b9b7357cb1fc074fe2237
Jony Flores <jonatan.flores@softvision.com>
2020-01-24 14:32:54 -0300

    Pagina de usuario logueado y guest

--- app/screens/Account/MyAccount.js
+++ app/screens/Account/MyAccount.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import * as firebase from "firebase";
 import { View, Text } from "react-native";
 import Loading from "../../components/Loading";
+import UserGuest from "./UserGuest";
+import UserLogged from "./UserLogged";
 
 export default function MyAccount() {
   const [login, setLogin] = useState(null);
@@ -19,17 +21,5 @@ export default function MyAccount() {
     return <Loading isVisible={true} text="Cargando..." />;
   }
 
-  if (login) {
-    return (
-      <View>
-        <Text>Usuario logueado... del if</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View>
-      <Text>Usuario no logueado...</Text>
-    </View>
-  );
+  return login ? <UserLogged /> : <UserGuest />;
 }
