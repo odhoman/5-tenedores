e71810409a8d03105ee0fcebb6ef614c39a3dec7
Jony Flores <jonatan.flores@softvision.com>
2020-01-23 21:26:17 -0300

    Se agregan paginas principales

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>5 Tenedores qqqqaaaa</Text>
+      <Text>5 Tenedores w21</Text>
     </View>
   );
 }
