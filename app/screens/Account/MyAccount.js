d97440a6c02bc2b15e191e7fd561a6c518753cc3
Jony Flores <jonatan.flores@softvision.com>
2020-01-24 14:24:16 -0300

    Se agrega el componente Loading

--- app/screens/Account/MyAccount.js
+++ app/screens/Account/MyAccount.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as firebase from "firebase";
 import { View, Text } from "react-native";
+import Loading from "../../components/Loading";
 
 export default function MyAccount() {
   const [login, setLogin] = useState(null);
@@ -15,11 +16,7 @@ export default function MyAccount() {
   console.log("login " + login);
 
   if (login === null) {
-    return (
-      <View>
-        <Text>Usuario NO logueado del if...</Text>
-      </View>
-    );
+    return <Loading isVisible={true} text="Cargando..." />;
   }
 
   if (login) {
