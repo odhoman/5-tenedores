4e25234c8a66be8d301dc70a89abebab4dabf080
Jony Flores <jonatan.flores@softvision.com>
2020-01-23 22:20:00 -0300

    Se abre la navegacion

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Navigation from "./app/navigations/Navigation";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>5 Tenedores...e e e z z</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>5 Tenedores...e e e z z z</Text>
+    // </View>
+    <Navigation />
   );
 }
 
