97e59cd0c5abef6b3b13f598542cc004521f82ae
Jony Flores <jonatan.flores@softvision.com>
2020-02-14 17:35:27 -0300

    Actualizando el avatar del usuario en su Información

--- app/screens/Account/InfoUser.js
+++ app/screens/Account/InfoUser.js
@@ -1,13 +1,64 @@
 import React, { useRef } from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import { Avatar } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImgagePicker from "expo-image-picker";
 
 export default function InfoUser(props) {
   const {
     userInfo: { uid, displayName, email, photoURL }
   } = props;
-  const changeAvatar = () => {
-    console.log("esta cambiando el avatar");
+  const changeAvatar = async () => {
+    const resultPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+    const resultPermissionCamera =
+      resultPermission.permissions.cameraRoll.status;
+    if (resultPermissionCamera === "denied") {
+      console.log("dar permisos a la camara");
+    } else if (resultPermissionCamera === "granted") {
+      //TODO restringir tamanio para que no sea gigante
+      const result = await ImgagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3]
+      });
+
+      if (result.cancelled) {
+        cosole.log("has cerrado la galeria de imagenes");
+      } else {
+        uploadImage(result.uri, uid).then(() => {
+          updatePhotoUrl(uid);
+        });
+      }
+    }
+  };
+
+  const updatePhotoUrl = uid => {
+    firebase
+      .storage()
+      .ref(`avatar/${uid}`)
+      .getDownloadURL()
+      .then(async result => {
+        const update = {
+          photoURL: result
+        };
+        await firebase.auth().currentUser.updateProfile(update);
+      })
+      .catch(e => {
+        console.log("Error recuperando url de imagen: " + e);
+      });
+  };
+
+  const uploadImage = async (uri, uid) => {
+    const response1 = await fetch(uri);
+    const blob = await response1.blob();
+    //console.log(blob);
+    const ref = firebase
+      .storage()
+      .ref()
+      .child(`avatar/${uid}`);
+    return ref.put(blob);
   };
 
   return (
